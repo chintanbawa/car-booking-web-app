944f9513c9c358c78d4e98371c8c39af09d46f26
chintanbawa <chintanbawa@gmail.com>
2021-08-25 01:19:17 +0530

    Implemented About Us page

---
+++ src/pages/About/AboutElements.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex; 
+    flex-direction: column;
+    align-items: center;
+    padding: 4rem calc((100vw - 1100px) / 2);
+    margin-left: 0px 24px;   
+`
